a9a7ced2f343c00458c60cf60c07b2c9a796a290
setografi <hitonaashbell@gmail.com>
2024-03-12 21:54:40 +0700

    first commit

---
+++ src/components/ParallexEvent.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Layer1 from "../assets/SUN.png";
+import Layer2 from "../assets/PX4.png";
+import Layer3 from "../assets/PX3.png";
+import Layer4 from "../assets/PX2.png";
+import Layer5 from "../assets/PX1.png";
+import Layer6 from "../assets/Samurai.png";
+import TextImage from "../assets/Yamitsuki.svg";
+
+function ParallexEvent() {
+  return (
+    <>
+      <div>
+        <img className="w-full h-[59.9rem] z-10" src={Layer1} alt="" />
+        <div>
+          <div className="absolute top-32 left-0 z-20 mix-blend-exclusion">
+            <img className="" src={TextImage} alt="" />
+          </div>
+          <img
+            className="absolute top-0 left-0 w-full h-[59.9rem] z-30"
+            src={Layer2}
+            alt=""
+          />
+          <div>
+            <img
+              className="absolute top-0 left-0 w-full h-[59.9rem] z-40"
+              src={Layer3}
+              alt=""
+            />
+            <div>
+              <img
+                className="absolute top-0 left-0 w-full h-[59.9rem] z-50"
+                src={Layer4}
+                alt=""
+              />
+              <div>
+                <img
+                  className="absolute top-0 left-0 w-full h-[59.9rem] z-[60]"
+                  src={Layer5}
+                  alt=""
+                />
+                <div>
+                  <img
+                    className="absolute top-0 left-0 w-full h-[59.9rem] z-[70]"
+                    src={Layer6}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ParallexEvent;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarEvent from "../components/NavbarEvent";
+import ParallexEvent from "../components/ParallexEvent";
+
+function HomePage() {
+  return (
+    <>
+      <NavbarEvent />
+      <ParallexEvent />
+    </>
+  );
+}
+
+export default HomePage;
